ec608a7ed7028561c31370d22fab92191795d4f3
mariuszbialas <106605256+mariuszbialas@users.noreply.github.com>
2022-12-11 14:06:39 +0100

    add border to the img and class bold to user.last_name

--- src/UserInfo/UserInfoOneItem.js
+++ src/UserInfo/UserInfoOneItem.js
@@ -6,8 +6,8 @@ export const UserInfoOneItem = ({user}) => {
         <div key={user.username} className={"users"}>
             <img src={`https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/${user.picture}`} alt="" className={"users__picture"}/>
             <div  className={"users__info"}>
-                <span className={"capitalize bold"}>{user.first_name} </span>
-                <span className={"capitalize"}>{user.last_name}</span>
+                <span className={"capitalize"}>{user.first_name} </span>
+                <span className={"capitalize bold"}>{user.last_name}</span>
                 <hr/>
                 <p>{user.username}</p>
             </div>
